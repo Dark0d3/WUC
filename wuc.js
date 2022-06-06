41c2589aec9c0bfa831852fc3bd80a32b8b35f17
Afreed C P <47804992+Dark0d3@users.noreply.github.com>
2022-06-06 10:45:31 +0530

    first commit

---
+++ wuc.js
@@ -0,0 +1,18 @@
+var sliderContent = document.getElementsById("slider")
+
+
+function firstImage(){
+    sliderContent.src="./WUC-assets/1";
+}
+function secondImage(){
+    sliderContent.src="./WUC-assets/2";
+}
+function thirdImage(){
+    sliderContent.src="./WUC-assets/3";
+}
+function fourthImage(){
+    sliderContent.src="./WUC-assets/4";
+}
+function fifthImage(){
+    sliderContent.src="./WUC-assets/5";
+}
